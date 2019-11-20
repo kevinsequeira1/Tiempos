80b5dfa70177302913638f16488fb63fc6381e46
kevinsequeira <kevinsequeira23@gmail.com>
2019-11-19 23:59:18 -0600

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,31 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAqiJWDCo3kuqWJrNokeald3x7KUjLScMk',
+    authDomain: 'tiempos-efc99.firebaseapp.com',
+    databaseURL: 'https://tiempos-efc99.firebaseio.com',
+    projectId: 'tiempos-efc99',
+    storageBucket: 'tiempos-efc99.appspot.com',
+    messagingSenderId: '279283002629'
+  }
 };
+
+//var firebaseConfig = {
+      //apiKey: "AIzaSyAqiJWDCo3kuqWJrNokeald3x7KUjLScMk",
+      //authDomain: "tiempos-efc99.firebaseapp.com",
+      //databaseURL: "https://tiempos-efc99.firebaseio.com",
+      //projectId: "tiempos-efc99",
+      //storageBucket: "tiempos-efc99.appspot.com",
+      //messagingSenderId: "279283002629",
+      //appId: "1:279283002629:web:cff22b7eca04529e491b6c",
+      //measurementId: "G-K1WBZWY5JN"
+    //};
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
